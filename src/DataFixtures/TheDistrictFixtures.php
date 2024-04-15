e6ac0c086707012aaed1e4c93e615e6e4dfed894
fabrice <fabricebeaujois@gmail.com>
2024-04-15 17:04:11 +0200

    fixtures_cat

--- src/DataFixtures/TheDistrictFixtures.php
+++ src/DataFixtures/TheDistrictFixtures.php
@@ -2,6 +2,13 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Utilisateur;
+use App\Entity\User;
+use App\Entity\Plat;
+use App\Entity\Categorie;
+use App\Entity\Commande;
+use App\Entity\Detail;
+
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +16,45 @@ class TheDistrictFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // $categorie1 = new Categorie();
 
+        // $categorie1->setLibelle('Pizza');
+        // $categorie1->setImage('image/category/pizza_cat.png');
+        // $categorie1->setActive('yes');
+
+        include 'truc.php';
+        $categorieRepo = $manager->getRepository(Categorie::class);
+
+        // foreach ($categorie as $cat) {
+        //     $categorieDB = new Categorie();
+        //     $categorieDB
+        //     ->setLibelle($cat['libelle'])
+        //     ->setImage($cat['image'])
+        //     ->setActive($cat['active']);
+
+        //     $manager->persist($categorieDB);
+
+        //     //empecher auto-increment//
+        //     $metadata = $manager->getClassMetaData(Categorie::class);
+        //     $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE);    
+        // }
+        // $manager->flush();
+        
+        foreach ($plat as $p) {
+            $platDB = new Plat();
+            $platDB
+            ->setLibelle($p['libelle'])
+            ->setDescription($p['description'])
+            ->setPrix($p['prix'])
+            ->setImage($p['image'])
+            ->setActive($p['active']);
+            // $categorie = $categorieRepo->find($p['categorie_id']);
+            // $platDB->setCategorie($categorie);
+            // $details = $detailsRepo->find($d['details_id']);
+            // $platDB->addDetail($details);
+            $manager->persist($platDB);            
+        }
         $manager->flush();
     }
 }
+
--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -40,6 +40,13 @@ class Categorie
         return $this->id;
     }
 
+    public function setId(int $id): static
+    {   
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getLibelle(): ?string
     {
         return $this->libelle;
