9e2fabfff4140793d8e5a9082f748208da066219
fabrice <fabricebeaujois@gmail.com>
2024-04-23 16:56:27 +0200

    23Av

--- src/Controller/LogerInterfaceControler.php
+++ src/Controller/LogerInterfaceControler.php
@@ -1,27 +1,27 @@
-<?php
+// <!--<?php
 
-namespace App\Controller;
+//namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Psr\Log\LoggerInterface;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LogExampleController extends AbstractController
-{
-    private $logger;
+// class LogExampleController extends AbstractController
+// {
+//     private $logger;
 
-    public function __construct(LoggerInterface $logger)
-    {
-        // Utilisez le service personnalisé si vous l'avez défini
-        $this->logger = $logger;
-    }
+//     public function __construct(LoggerInterface $logger)
+//     {
+//         // Utilisez le service personnalisé si vous l'avez défini
+//         $this->logger = $logger;
+//     }
 
-    public function index(): Response
-    {
-        $this->logger->info('Page d\'accueil visitée.');
+//     public function index(): Response
+//     {
+//         $this->logger->info('Page d\'accueil visitée.');
 
-        // Votre logique de contrôleur ici
+//         // Votre logique de contrôleur ici
 
-        return $this->render('default/index.html.twig');
-    }
-}
+//         return $this->render('default/index.html.twig');
+//     }
+//} 
